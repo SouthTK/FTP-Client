be9363cbdbe4eda33be2ae744e64fa51a701b687
unknown <khanhnam176@gmail.com>
2026-05-09 19:51:31 +0700

    move button event to executor service for robustness

--- run.bat
+++ run.bat
@@ -2,3 +2,5 @@ cd src
 javac --module-path "../../../javafx-sdk-17.0.17/lib;." --add-modules javafx.base,javafx.controls,javafx.fxml,javafx.graphics,javafx.media,javafx.swing,javafx.web App.java
 java --module-path "../../../javafx-sdk-17.0.17/lib;." --add-modules javafx.base,javafx.controls,javafx.fxml,javafx.graphics,javafx.media,javafx.swing,javafx.web App
 cd ..
+
+:: -Xlint:unchecked
--- src/ui/MainPanel.java
+++ src/ui/MainPanel.java
@@ -17,14 +17,23 @@ import javafx.geometry.Insets;
 
 import javafx.application.Platform;
 
+import java.util.concurrent.ExecutorService; 
+import java.util.concurrent.Executors;
+
 import background.ProcessThread;
 
 public class MainPanel extends BorderPane {
     private SceneManager manager;
     private ProcessThread process;
+    private ExecutorService pool;
+
+    private Component.CustomButton currentDirectory;
+    private TextArea outputBox;
+    private Component.CustomView directoryView;
 
     public MainPanel(SceneManager manager) {  
         this.manager = manager; 
+        this.pool = Executors.newFixedThreadPool(1);
         this.setUpTop(); 
         this.setUpLeft();
         this.setUpCenter(); 
@@ -39,25 +48,19 @@ public class MainPanel extends BorderPane {
 
     public void updateCurrentDirectory(String input) {
         Platform.runLater(() -> {
-            VBox box = (VBox) this.getLeft();
-            Component.CustomButton currentDirectory = (Component.CustomButton) box.getChildren().get(0);
-            currentDirectory.setText(input);
+            this.currentDirectory.setText(input);
         });
     }
 
     public void updateOutput(String input) {
         Platform.runLater(() -> {
-            VBox box = (VBox) this.getBottom();
-            TextArea outputBox = (TextArea) box.getChildren().get(0);
-            outputBox.appendText(input + System.lineSeparator());
+            this.outputBox.appendText(input + System.lineSeparator());
         });    
     }
 
     public void clearOutput() {
         Platform.runLater(() -> {
-            VBox box = (VBox) this.getBottom();
-            TextArea outputBox = (TextArea) box.getChildren().get(0);
-            outputBox.clear();
+            this.outputBox.clear();
         });   
     }
 
@@ -71,24 +74,19 @@ public class MainPanel extends BorderPane {
                 for (int i = 9; i < parts.length; i++) {name += " " + parts[i];}
             }
 
-            Component.FileInfo info = new Component.FileInfo(parts[0], parts[1], parts[2], parts[3], 
-                    parts[4], date, name);
-
-            VBox box = (VBox) this.getLeft();
-            Component.CustomView directoryView = (Component.CustomView) box.getChildren().get(1);
-            directoryView.getItems().add(info);
+            Component.FileInfo info = new Component.FileInfo(parts[0], 
+                    parts[1], parts[2], parts[3], parts[4], date, name);
+            this.directoryView.getItems().add(info);
         });
     }
 
     public void clearDirectory(boolean fullClear) {
-        VBox box = (VBox) this.getLeft();
-        Component.CustomView directoryView = (Component.CustomView) box.getChildren().get(1);
-        directoryView.getItems().clear();
+        this.directoryView.getItems().clear();
         if (!fullClear) {
             Component.FileInfo parent = new Component.FileInfo(" ", "", "", "", "", "", "..");
             Component.FileInfo root = new Component.FileInfo(" ", "", "", "", "", "", "/");
-            directoryView.getItems().add(root);
-            directoryView.getItems().add(parent);
+            this.directoryView.getItems().add(root);
+            this.directoryView.getItems().add(parent);
         }
     }
 
@@ -102,27 +100,31 @@ public class MainPanel extends BorderPane {
         connectButton.setText("Connect");
         connectButton.setPrefWidth(80);
         connectButton.setOnAction(event -> {
-            this.process.connect(addressBox.getText());
+            pool.execute(() -> {
+                this.process.connect(addressBox.getText());
+            });
         });
 
         Component.CustomButton disconnectButton = new Component.CustomButton();
         disconnectButton.setText("Disconnect");
         disconnectButton.setPrefWidth(80);
         disconnectButton.setOnAction(event -> {
-            this.process.disconnect();
-            this.updateCurrentDirectory("");
-            this.clearDirectory(true);
+            pool.execute(() -> {
+                this.process.disconnect();
+                this.updateCurrentDirectory("");
+                this.clearDirectory(true);
+            });
         });
 
         Component.CustomButton logoutButton = new Component.CustomButton();
         logoutButton.setText("Log out");
         logoutButton.setPrefWidth(80);
         logoutButton.setOnAction(event -> {
-            this.process.disconnect();
-            this.manager.setToLoginScene();
-            this.updateCurrentDirectory("");
-            this.clearDirectory(true);  
-            this.clearOutput();
+                this.process.disconnect();
+                this.manager.setToLoginScene();
+                this.updateCurrentDirectory("");
+                this.clearDirectory(true);  
+                this.clearOutput();
         });
 
         Region spacer = new Region();
@@ -139,16 +141,15 @@ public class MainPanel extends BorderPane {
                 + "-fx-background-repeat: no-repeat; "
                 + "-fx-background-position: center top; " 
                 + "-fx-background-size: 400 70;");
-                
         this.setTop(topLayout);
     }
 
     private void setUpLeft() {
-        Component.CustomButton currentDirectory = new Component.CustomButton();
-        currentDirectory.setText("");
-        currentDirectory.setPrefWidth(200);
+        this.currentDirectory = new Component.CustomButton();
+        this.currentDirectory.setText("");
+        this.currentDirectory.setPrefWidth(200);
 
-        Component.CustomView directoryView = new Component.CustomView();
+        this.directoryView = new Component.CustomView();
 
         VBox leftLayout = new VBox(10);
         leftLayout.getChildren().addAll(currentDirectory, directoryView); 
@@ -171,12 +172,13 @@ public class MainPanel extends BorderPane {
         deleteButton.setPrefWidth(preWidth);
         deleteButton.setOnAction(event -> {
             VBox box = (VBox) this.getLeft();
-            TableView<Component.FileInfo> directoryView = (TableView<Component.FileInfo>) box.getChildren().get(1);
             Component.FileInfo selectedFile = directoryView.getSelectionModel().getSelectedItem();
 
             if (selectedFile != null) {
-                String fileName = selectedFile.getName();
-                this.process.deleteFile(fileName);
+                pool.execute(() -> {
+                    String fileName = selectedFile.getName();
+                    this.process.deleteFile(fileName);
+                });
             }
         });
 
@@ -185,12 +187,13 @@ public class MainPanel extends BorderPane {
         directoryButton.setPrefWidth(preWidth);
         directoryButton.setOnAction(event -> {
             VBox box = (VBox) this.getLeft();
-            TableView<Component.FileInfo> directoryView = (TableView<Component.FileInfo>) box.getChildren().get(1);
             Component.FileInfo selectedFile = directoryView.getSelectionModel().getSelectedItem();
 
-            if (selectedFile != null) {
-                String directory = selectedFile.getName();
-                this.process.changeDirectory(directory);
+            if (selectedFile != null) {    
+                pool.execute(() -> {
+                    String directory = selectedFile.getName();
+                    this.process.changeDirectory(directory);
+                });
             }
         });
 
@@ -213,12 +216,13 @@ public class MainPanel extends BorderPane {
         rmdirButton.setPrefWidth(preWidth);
         rmdirButton.setOnAction(event -> {
             VBox box = (VBox) this.getLeft();
-            TableView<Component.FileInfo> directoryView = (TableView<Component.FileInfo>) box.getChildren().get(1);
             Component.FileInfo selectedFile = directoryView.getSelectionModel().getSelectedItem();
 
             if (selectedFile != null) {
-                String directory = selectedFile.getName();
-                this.process.deleteFolder(directory);
+                pool.execute(() -> {
+                    String directory = selectedFile.getName();
+                    this.process.deleteFolder(directory);
+                });
             }  
         });
 
@@ -245,10 +249,10 @@ public class MainPanel extends BorderPane {
     }
 
     private void setUpBottom() {
-        TextArea outputBox = new TextArea();
-        outputBox.setStyle("-fx-control-inner-background: rgba(30, 30, 30, 1);");
-        outputBox.setEditable(false); 
-        outputBox.setPromptText("Output will appear here...");
+        this.outputBox = new TextArea();
+        this.outputBox.setStyle("-fx-control-inner-background: rgba(30, 30, 30, 1);");
+        this.outputBox.setEditable(false); 
+        this.outputBox.setPromptText("Output will appear here...");
 
         VBox bottomLayout = new VBox(10);
         bottomLayout.getChildren().addAll(outputBox); 
